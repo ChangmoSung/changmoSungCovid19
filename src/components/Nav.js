60b1d2259d00a785eab41c9871c38fbb3eeffa47
ChangmoSung <changmosung0914@gmail.com>
2020-04-01 20:46:48 -0400

    function to toggle nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,31 +8,42 @@ class Nav extends Component {
         super();
 
         this.state = {
-
+            navOpened: false,
         }
     }
+
+    toggleNav = () => {
+        this.setState({
+            navOpened: !this.state.navOpened,
+        })
+
+        document.querySelector('nav').classList.toggle('showNav');
+    }
+
     render() { 
         return ( 
             <nav className='nav'>
+                <button className='navTrigger' onClick={this.toggleNav}>X</button>
+
                 <ul>
                     <li>
-                        <Link to='/changmoSungCovid19/'>HOME</Link>
+                        <Link to='/changmoSungCovid19/' onClick={this.toggleNav}>HOME</Link>
                     </li>
 
                     <li>
-                        <Link to='/changmoSungCovid19/info/aboutCovid19'>ABOUT COVID-19</Link>
+                        <Link to='/changmoSungCovid19/info/aboutCovid19' onClick={this.toggleNav}>COVID-19</Link>
                     </li>
 
                     <li>
-                        <Link to='/changmoSungCovid19/info/selfcheck'>SELF-CHECK</Link>
+                        <Link to='/changmoSungCovid19/info/selfcheck' onClick={this.toggleNav}>SELF-CHECK</Link>
                     </li>
 
                     <li>
-                        <Link to='/changmoSungCovid19/info/statistic'>STATISTIC</Link>
+                        <Link to='/changmoSungCovid19/info/statistic' onClick={this.toggleNav}>STATISTIC</Link>
                     </li>
 
                     <li>
-                        <Link to='/changmoSungCovid19/info/news'>NEWS</Link>
+                        <Link to='/changmoSungCovid19/info/news' onClick={this.toggleNav}>NEWS</Link>
                     </li>
                 </ul>
             </nav>
